69d600628431f889098b6c357508ac688c710c50
d11210920 <d11210920@naver.com>
2024-08-28 17:24:50 +0900

    feat - 카테고리 분류 로직 생성

---
+++ src/main/java/icurriculum/domain/categoryjudge/CategoryJudgeUtils.java
@@ -0,0 +1,12 @@
+package icurriculum.domain.categoryjudge;
+
+import icurriculum.domain.curriculum.Curriculum;
+import icurriculum.domain.membermajor.MemberMajor;
+import icurriculum.domain.take.Category;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategoryJudgeUtils {
+    Map<String,Category> judge(List<String> codes, Curriculum curriculum);
+}
