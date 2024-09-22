6e01db9b580943f5b28ab7e3b6303d854613b8b0
d11210920 <d11210920@naver.com>
2024-09-22 16:28:54 +0900

    feat : getCourse 함수 작성
    단일 course 조회 함수 작성했습니다.
    
    Resolved : #7

--- src/main/java/icurriculum/domain/course/service/CourseService.java
+++ src/main/java/icurriculum/domain/course/service/CourseService.java
@@ -1,13 +1,21 @@
 package icurriculum.domain.course.service;
 
+import icurriculum.domain.categoryjudge.CategoryJudgeUtils;
+import icurriculum.domain.categoryjudge.CategoryJudgeUtilsImpl;
 import icurriculum.domain.course.Course;
+import icurriculum.domain.course.dto.CourseSearchResponseDTO;
 import icurriculum.domain.course.repository.CourseRepository;
+import icurriculum.domain.curriculum.Curriculum;
+import icurriculum.domain.curriculum.service.CurriculumService;
+import icurriculum.domain.member.Member;
+import icurriculum.domain.membermajor.MemberMajor;
+import icurriculum.domain.membermajor.service.MemberMajorService;
+import icurriculum.domain.take.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +23,9 @@ import java.util.Set;
 public class CourseService {
 
     private final CourseRepository repository;
+    private final CurriculumService curriculumService;
+    private final CategoryJudgeUtils categoryJudgeUtils;
+    private final MemberMajorService memberMajorService;
 
     public List<Course> findCoursesByCodes(Set<String> codes) {
         return repository.findByCodes(codes);
@@ -26,5 +37,22 @@ public class CourseService {
         /**
          * TODO 예외 추후 정의
          */
+    }
+
+    public CourseSearchResponseDTO getCourse(Member member, String code) {
+        List<MemberMajor> majorsByMember = memberMajorService.findMajorsByMember(member);
+        Curriculum curriculum = curriculumService.findCurriculumByMemberMajor(majorsByMember);
+        Optional<Course> course = repository.findByCode(code);
+        if(course.isEmpty()) throw new RuntimeException("해당 과목이 없습니다.");
+        Course findCourse = course.get();
+        ArrayList<String> codes = new ArrayList<>();
+        codes.add(code);
+        Map<String, Category> judgedCodes = categoryJudgeUtils.judge(codes, curriculum);
+        return CourseSearchResponseDTO.builder()
+                .courseId(findCourse.getId())
+                .name(findCourse.getName())
+                .credit(findCourse.getCredit())
+                .category(judgedCodes.get(code).toString())
+                .build();
     }
 }
