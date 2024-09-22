790965837712b246d3998d10828e05d9a92bbfe8
park_yeongyu <sjjs7771@naver.com>
2024-09-22 18:27:17 +0900

    feat : 학과 이름을 통한 학과 가져오는 기능 추가

--- src/main/java/icurriculum/domain/department/repository/DepartmentRepository.java
+++ src/main/java/icurriculum/domain/department/repository/DepartmentRepository.java
@@ -1,7 +1,10 @@
 package icurriculum.domain.department.repository;
 
 import icurriculum.domain.department.Department;
+import icurriculum.domain.department.DepartmentName;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> findByName(DepartmentName name);
 }
