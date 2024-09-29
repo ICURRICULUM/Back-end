8109d4fc334ca8b462e0e8a957afa4832b65bff3
d11210920 <d11210920@naver.com>
2024-09-29 18:09:31 +0900

    [Feat] Category 판별 로직 생성
    CategoryJudgeUtilsImpl 내의 judge 함수 실행시 파라미터로 넣은 code들에 대한 code,Category key-value Map을 반환
    
    Resolves: #25

---
+++ src/main/java/icurriculum/domain/CategoryJudge/CategoryJudgeUtils.java
@@ -0,0 +1,11 @@
+package icurriculum.domain.categoryjudge;
+
+import icurriculum.domain.curriculum.Curriculum;
+import icurriculum.domain.take.Category;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategoryJudgeUtils {
+    Map<String,Category> judge(List<String> codes, Curriculum curriculum);
+}
