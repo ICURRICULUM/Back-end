d7fc983e79d02b822015726944ac44d8490ce1b1
kseokc <alvinseok@naver.com>
2024-10-12 17:58:27 +0900

    feat : Take 성적관련 Grade enum 추가
    - Take 관련해서 APLUS ~ PASS 까지 enum 타입으로 추가

---
+++ src/main/java/icurriculum/domain/take/Grade.java
@@ -0,0 +1,37 @@
+package icurriculum.domain.take;
+
+import java.util.Arrays;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Grade {
+    A_PLUS("A+", 4.5),
+    A_ZERO("A0", 4.0),
+    B_PLUS("B+", 3.5),
+    B_ZERO("B0", 3.0),
+    C_PLUS("C+", 2.5),
+    C_ZERO("C0", 2.0),
+    D_PLUS("D+", 1.5),
+    D_ZERO("D0", 1.0),
+    FAIL("F", 0.0),
+    PASS("P", -1.0); //사용시 주의 하세요
+
+    private final String string;
+    private final Double score;
+
+    public static Grade getGrade(String name) {
+        return Arrays.stream(Grade.values())
+            .filter(grade -> grade.getString().equals(name))
+            .findFirst()
+            .orElse(Grade.PASS);
+    }
+
+    public static Grade getGradeByScore(Double score) {
+        return Arrays.stream(Grade.values())
+            .filter(grade -> grade.getScore().equals(score))
+            .findFirst()
+            .orElse(Grade.PASS);  // 찾지 못하면 기본값으로 PASS를 반환
+    }
+}
